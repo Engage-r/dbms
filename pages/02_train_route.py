dd4ea12a5d08a7899e61185859e284c471434905
SyedMa3 <sammateen10@outlook.com>
2023-05-04 15:07:54 +0530

    cleaned code

--- pages/02_train_route.py
+++ pages/02_train_route.py
@@ -8,22 +8,38 @@ def show_train_route():
     # Get the train number from the user
     train_number = st.text_input("Enter train number:")
 
-    conn = psycopg2.connect(
+    if st.button("Show Train Route"):
+        conn = psycopg2.connect(
         host="localhost",
         database="t2",
         user="postgres",
         password="password"
     )
 
-    # create a cursor object to execute SQL queries
-    cur = conn.cursor()
 
-    
-    cur.execute("SELECT * FROM train_stations where train_no=%s", (int(train_number),))
-    # train_routes = cur.fetchall()
+        # create a cursor object to execute SQL queries
+        cur = conn.cursor()
 
-    train_routes = pd.DataFrame(cur.fetchall(), columns=["Train no", "Station Name"])
-    # Show the train route if the user has entered a valid train number
+        
+        cur.execute("SELECT * FROM train_stations where train_no=%s", (int(train_number),))
+        # train_routes = cur.fetchall()
+
+        train_routes = pd.DataFrame(cur.fetchall(), columns=["Train no", "Station Name"])
+
+                # Split the station names using str.split() method
+        train_routes["Stations"] = train_routes["Station Name"].str.split(",")
+
+        # Use explode() to create a new row for each station
+        train_routes = train_routes.explode("Stations")
+
+        # Drop unnecessary columns
+        train_routes.drop(columns=["Train no", "Station Name"], inplace=True)
+        print(train_routes)
+        # Show the train route if the user has entered a valid train number
+        if not train_routes.empty:
+            st.table(train_routes)
+
+        # Show the train route if the user has entered a valid train number
 
 
 # Create a page to show the train route
