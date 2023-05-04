310a3ee0d2a49f0452f496ab0d78c8cda17038ab
SyedMa3 <sammateen10@outlook.com>
2023-05-04 10:24:37 +0530

    added booked tickets

--- pages/booked_ticket.py
+++ pages/booked_ticket.py
@@ -1,19 +1,34 @@
 import streamlit as st
 import pandas as pd
+import psycopg2
 
 # define the booked tickets page
 def booked_tickets_page(user_email):
     st.header("Booked Tickets")
 
     # load the ticket data from the CSV file
-    tickets_df = pd.read_csv("tickets.csv")
+    user_id = st.session_state["user"]
 
-    # filter the ticket data by the user's email address
-    user_tickets_df = tickets_df[tickets_df["Email"]==user_email]
+    conn = psycopg2.connect(
+        host="localhost",
+        database="t2",
+        user="postgres",
+        password="password"
+    )
+
+    # create a cursor object to execute SQL queries
+    cur = conn.cursor()
+
+    # execute the SQL query to retrieve all stations
+    cur.execute("SELECT * FROM ticket WHERE user_id = %s", (int(user_id),))
+
+    user_tickets_df = pd.DataFrame(cur.fetchall(), columns=["PNR", "Src Station Name", "Dest Station Name", "Train No", "User_id", "Date", "No of seats"])
+
+# add a "Cancel" button column to the dataframe using the apply method
 
     # display the ticket data in a table
     if len(user_tickets_df) > 0:
-        st.write(user_tickets_df[["Ticket ID", "Source", "Destination", "Date", "Seats"]])
+        st.write(user_tickets_df[["PNR", "Src Station Name", "Dest Station Name", "Train No","Date", "No of seats"]])
     else:
         st.write("You have not booked any tickets yet.")
 
