1eb56c91afb222fe28f44eda1ae091faff39b75c
SyedMa3 <sammateen10@outlook.com>
2023-05-04 12:23:55 +0530

    backend code for route

--- pages/cancel_ticket.py
+++ pages/cancel_ticket.py
@@ -1,21 +1,42 @@
 import streamlit as st
 import pandas as pd
+import psycopg2
+
+psycopg2.extras.register_uuid()
+import uuid
 
 # define the cancel ticket page
 def cancel_ticket_page():
     st.header("Cancel Ticket")
 
     # get user input for ticket ID
-    ticket_id = st.text_input("Enter ticket ID:")
+    pnr = st.text_input("Enter ticket ID:")
+    conn = psycopg2.connect(
+        host="localhost",
+        database="t2",
+        user="postgres",
+        password="password"
+    )
 
-    # check if the ticket ID is valid and cancel the ticket
-    tickets_df = pd.read_csv("tickets.csv") # assuming the ticket data is stored in a CSV file
-    if ticket_id in tickets_df["Ticket ID"].values:
-        tickets_df = tickets_df[tickets_df["Ticket ID"]!=ticket_id]
-        tickets_df.to_csv("tickets.csv", index=False) # update the ticket data CSV file
-        st.success("Ticket successfully cancelled!")
-    elif ticket_id != "":
-        st.error("Invalid ticket ID. Please try again.")
+    # create a cursor object to execute SQL queries
+    cur = conn.cursor()
+
+    if st.button("Cancel Ticket"):
+        pnr = uuid.UUID(pnr)
+
+        # execute the SQL query to retrieve all stations
+        cur.execute("SELECT * FROM ticket where pnr=%s", (pnr,))
+        row = cur.fetchone()
+        print(row)
+        print("++++++++++++++++++===")
+        # check if the ticket ID is valid and cancel the ticket
+        
+        if row:
+            cur.execute("DELETE FROM  ticket WHERE pnr = %s", (pnr,))
+            conn.commit()
+            st.success("Ticket successfully cancelled!")
+        else:
+            st.error("Invalid ticket ID. Please try again.")
 
 # run the app
 if __name__ == "__main__":
